acf4d2b33d315611a12443ee3e363ba2716ac777
tylerstauss <tyler.e.stauss@gmail.com>
2014-06-04 15:34:54 -0700

    initializing for heroku

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubbygems.org"
+
+gem 'sinatra'
+gem 'json'
+gem 'httparty'
+gem 'pony'
--- app.rb
+++ app.rb
@@ -2,19 +2,28 @@ require 'sinatra'
 require 'json'
 require 'uri'
 require 'httparty'
+require 'pony'
 
 get '/' do
 	"Hello World"
-	@result = HTTParty.post('/api/send-email', 
-    :body => { :subject => 'This is the screen name', 
-               :issue_type => 'Application Problem', 
-               :status => 'Open', 
-               :priority => 'Normal', 
-               :description => 'This is the description for the problem'
-             }.to_json,
-    :headers => { 'Content-Type' => 'application/json' } )
+	# HTTParty.post('/api/send-email', 
+ #    :body => { :to => 'tyler.e.stauss@gmail.com', 
+ #               :subject => 'Hello World', 
+ #               :body => 'Hi Ian! Sending you an email via this API I just made.'
+ #             }.to_json,
+ #    :headers => { 'Content-Type' => 'application/json' } )
 end
 
-post '/api/send-email' do
-	{ "to": "Ian Olsen <iolsen@newrelic.com>", "subject": "hello world", "body": "Hi Ian! Sending you an email via this API I just made." }
+get '/api/send-email' do
+	# @response = JSON.parse(request.body.to_s)
+	# p @response
+	puts 'email not sent'
+	Pony.mail({
+						:to => 'tyler.e.stauss@gmail.com',
+						:from => 'noreply@example.com',
+						:subject => 'hello world',
+						:body => 'Hi Ian! Sending you an email via this API I just made.',
+						:via => :sendmail
+						}) 
+	puts 'email sent'
 end
