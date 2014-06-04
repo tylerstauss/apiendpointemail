204508d413a27990f50958c8a711b96d0ef64b1f
tylerstauss <tyler.e.stauss@gmail.com>
2014-06-04 16:07:49 -0700

    updating web.rb pony options

--- web.rb
+++ web.rb
@@ -10,8 +10,8 @@ Pony.options = {
     :address => 'smtp.sendgrid.net',
     :port => '587',
     :domain => 'heroku.com',
-    :user_name => 'app26028351@heroku.com',
-    :password => 'ehwyfdfq',
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
     :authentication => :plain,
     :enable_starttls_auto => true
   }
@@ -19,12 +19,7 @@ Pony.options = {
 
 get '/' do
 	"Hello World"
-	# HTTParty.post('/api/send-email', 
- #    :body => { :to => 'tyler.e.stauss@gmail.com', 
- #               :subject => 'Hello World', 
- #               :body => 'Hi Ian! Sending you an email via this API I just made.'
- #             }.to_json,
- #    :headers => { 'Content-Type' => 'application/json' } )
+
 end
 
 get '/api/send-email' do
